7938a33e75693d1db8a54d76e2ed29f81b99eb0f
Pranita1207 <pranitajadhav2004@gmail.com>
2025-08-17 16:32:07 +0530

    Delete Assignment 6 directory

--- Assignment 6/Q3-Pascal'sTriangle.py
+++
@@ -1,7 +0,0 @@
-n=4
-for i in range(n):
-    val=1
-    for j in range(i+1):
-        print(val,end=' ')
-        val =val *(i -j ) // (j +1)
-    print()
--- Assignment 6/Q5-PyramidOfStars.py
+++
@@ -1,4 +0,0 @@
-n=5
-for i in range(n):
-    print(' ' * (n-i-1), end=' ')
-    print('* ' * (i+1))
